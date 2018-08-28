e5a24be3616b12f0af3dc3a0e683dde78e4b2f2e
Illya <harer440@gmail.com>
2018-08-28 18:05:11 +0300

    first commit

---
+++ src/OneController.java
@@ -0,0 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class OneController {
+   @FXML
+   Button btn;
+  public void click(){
+      System.out.println("ssdsdsdsds");
+  }
+}
